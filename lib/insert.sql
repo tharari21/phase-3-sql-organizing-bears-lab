427df931cb605fe52daf8601c67fe5a7fa4bee5a
Tomer Harari <tharari21@gmail.com>
2022-08-15 14:41:14 -0400

    commit tree 6808489b3e70d8c349e41bd9dc54900710bca766

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Mr. Chocolate', 15, 'Male', 'Black','temp', 1);
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Rowdy', 15, 'Male', 'Black','temp', 1); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Tabitha', 15, 'Male', 'Black','temp', 1); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Sargeant Brown', 15, 'Male', 'Black','temp', 1); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Melissa', 15, 'Male', 'Black','temp', 1); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Grinch', 15, 'Male', 'Black','temp', 1); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Wendy', 15, 'Male', 'Black','temp', 1); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES (NULL, 15, 'Male', 'Black','temp', 1);
